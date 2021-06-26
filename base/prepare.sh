958fc3d28416955bf0112bf97082cbf4e30440df
Luke Bloy <luke.bloy@gmail.com>
2021-06-25 20:40:47 -0400

    -viz -> -plot

--- base/prepare.sh
+++ base/prepare.sh
@@ -28,8 +28,8 @@ fi
 
 if [ "$START_XVFB" ]; then
     /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid \
-    --make-pidfile --background --exec /usr/bin/Xvfb -- :99 \
-    -screen 0 1400x900x24 -ac +extension GLX +render -noreset
+        --make-pidfile --background --exec /usr/bin/Xvfb -- :99 \
+        -screen 0 1400x900x24 -ac +extension GLX +render -noreset
     sleep 3
     export DISPLAY=:99
 fi

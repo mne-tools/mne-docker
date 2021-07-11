b435d710a29dc5d29ce547ad7fa3693b4040fc87
Robert Luke <hello@robertluke.net>
2021-07-11 19:45:24 +1000

    Test failing

--- tests/base.py
+++ tests/base.py
@@ -4,4 +4,4 @@ import os
 fnirs_data_folder = mne.datasets.fnirs_motor.data_path()
 fnirs_cw_amplitude_dir = os.path.join(fnirs_data_folder, 'Participant-1')
 raw_intensity = mne.io.read_raw_nirx(fnirs_cw_amplitude_dir, verbose=True)
-assert raw_intensity.get_data().shape == (56, 23239)
+assert raw_intensity.get_data().shape == (6, 23239)
